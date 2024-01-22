b8ad5da856cc0184258f60eeb41be88fc05de275
unknown <forkadulislam@gmail.com>
2024-01-23 03:42:07 +0600

    Fix default route for backend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,13 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Laravel App</title>
-    <!-- Include the compiled Vue.js assets -->
+    <title>Laravel App</title>
 </head>
 <body>
-<!-- Your existing Laravel content -->
-<div id="app"></div>
-<script src="{{ asset('js/app.js') }}" defer></script>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<h4>Backend is live....</h4>
 </body>
 </html>
